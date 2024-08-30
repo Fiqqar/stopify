ca47188147f63473237798c8113cf760966beb98
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-08-30 20:32:25 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-black h-screen">
+    <div class="flex" style="height:88vh;">
+      <!-- nav -->
+      <div class="w-56 bg-black h-full">
+        <div class="p-6">
+          <img src="./assets/logo.png" alt="" class="h-10">
+        </div>
+      </div>
+      <!-- main content -->
+       <div class="w-full h-full relative bg-blue-400">
+        <div class="w-full sticky top-0 p-2"></div>
+       </div>
+    </div>
+    <!-- play bar -->
+     <div class="w-full bg-light">
+      
+     </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
