536d42096c2382a1f7717dafa644672f631aa17f
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-09-08 12:18:17 +0700

    8-9-24 (add some button for main-content)
    done

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,15 @@
           {name: "Skiptracing"},
           {name: "Timeline"},
           {name: "Going Going Gone"},
+        ],
+        showdropdown: false,
+        recents: [
+          {src: "https://i.scdn.co/image/ab67616d0000b273fadca79809c06f6afe7de0d9", title: "Lampu Kuning", artist: "Juicy Luicy"},
+          {src: "https://i.scdn.co/image/ab67616d0000b27349bdf0e981cbba25d48b44e0", title: "Berdansalah, Karir Ini Tak Ada Artinya", artist: "Hindia"},
+          {src: "https://i.scdn.co/image/ab67616d0000b273c607bcd8355681ab4fac2968", title: "Am I Bothering You?", artist: "Reality Club"},
+          {src: "https://f4.bcbits.com/img/a0915963026_65", title: "Me Myself and Dollar Hell", artist: "Mild High Club"},
+          {src: "https://i1.sndcdn.com/artworks-WNkaw1v8ZzPLGoMp-PsTBCQ-t500x500.jpg", title: "No Suprises", artist: "Radiohead"},
+          {src: "https://i.scdn.co/image/ab67616d0000b2731bd6d088d3d81972af4cb81d", title: "Unititled", artist: "Rex Orange Country"}
         ]
       }
     }
@@ -76,8 +85,35 @@
         <div class="w-full sticky top-0 p-2 flex items-center justify-between">
           <div class="flex items-center">
             <button class="rounded-full bg-black w-8 h-8 text-white">
-              <i class="material-icons text-xl"></i>
+              <i class="material-icons text-xl mr-3">keyboard_arrow_left</i>
             </button>
+            <button class="rounded-full bg-black w-8 h-8 text-white">
+              <i class="material-icons text-xl">keyboard_arrow_right</i>
+            </button>
+          </div>
+          <div class="relative">
+              <button class="bg-light rounded-full py-1 px-2 flex items-center" @click="showdropdown = true">
+                <img class="rounded-full h-6 w-6" src="" alt="">
+                <p class="text-white font-semibold text-xs">fiq</p>
+                <i class="material-icons text-white">arrow_drop_down</i>
+              </button>
+              <div class="absolute bg-light w-full rounded mt" v-if="showdropdown === true">
+                <button class="w-full text-sm py-2 text-lightest hover text-white border-b border-light" @click="showdropdown = false">Account</button>
+                <button class="w-full text-sm py-2 text-lightest hover text-white border-b border-light" @click="showdropdown = false">Log out</button>
+              </div>
+            </div>
+        </div>
+        <div class="px-6 py-3 flex items-center container justify-between">
+          <h1 class="pl-2 text-2xl font-semibold text-white tracking-wider hover:underline">Recently Played</h1>
+          <h2 class="pr-8 pt-4 text-xs text-lightest uppercase tracking-wider hover:underline">See All</h2>
+          <div class="w-full flex-wrap">
+            <div class="p-2 w-48">
+              <div class="bg-light w-full h-auto p-5">
+                <img src="" alt="" class="h-auto w-full shadow mb-2">
+                <h1 class="text-sm font-semibold text-white tracking-wide"></h1>
+                <h2 class="text-xs text-lightest tracking-wide"></h2>
+              </div>
+            </div>
           </div>
         </div>
        </div>
