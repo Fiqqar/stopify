95af18808a6997e8edfaab2b23498547876fa371
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-09-13 20:31:37 +0700

    13-9-24 (adding recent and material symbols)
    done

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,12 @@
         ],
         showdropdown: false,
         recents: [
-          {src: "https://i.scdn.co/image/ab67616d0000b273fadca79809c06f6afe7de0d9", title: "Lampu Kuning", artist: "Juicy Luicy"},
-          {src: "https://i.scdn.co/image/ab67616d0000b27349bdf0e981cbba25d48b44e0", title: "Berdansalah, Karir Ini Tak Ada Artinya", artist: "Hindia"},
-          {src: "https://i.scdn.co/image/ab67616d0000b273c607bcd8355681ab4fac2968", title: "Am I Bothering You?", artist: "Reality Club"},
-          {src: "https://f4.bcbits.com/img/a0915963026_65", title: "Me Myself and Dollar Hell", artist: "Mild High Club"},
-          {src: "https://i1.sndcdn.com/artworks-WNkaw1v8ZzPLGoMp-PsTBCQ-t500x500.jpg", title: "No Suprises", artist: "Radiohead"},
-          {src: "https://i.scdn.co/image/ab67616d0000b2731bd6d088d3d81972af4cb81d", title: "Unititled", artist: "Rex Orange Country"}
+          {src: "/src/assets/image/lampu_kuning.jpeg", title: "Lampu Kuning", artist: "Juicy Luicy"},
+          {src: "/src/assets/image/berdansalah.jpeg", title: "Berdansalah, Karir Ini Tak Ada Artinya", artist: "Hindia"},
+          {src: "/src/assets/image/botheringyou.jpeg", title: "Am I Bothering You?", artist: "Reality Club"},
+          {src: "/src/assets/image/me_myself.jpeg", title: "Me Myself and Dollar Hell", artist: "Mild High Club"},
+          {src: "/src/assets/image/no_suprises.jpg", title: "No Suprises", artist: "Radiohead"},
+          {src: "/src/assets/image/untitled.jpeg", title: "Untitled", artist: "Rex Orange Country"}
         ]
       }
     }
@@ -32,16 +32,18 @@
 </script>
 
 <template>
+<!-- <span class="material-symbols-outlined">home</span> -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <div class="bg-black" style="height:100vh;">
     <div class="flex" style="height:88vh;">
       <!-- nav -->
       <div class="w-56 bg-black h-full">
         <div class="p-6">
-          <img src="./assets/logo.png" alt="" class="h-10">
+          <img src="./assets/logoname.png" alt="" class="h-20">
         </div>
         <div class="mx-2">
-          <button v-for="page in pages" @click="setid=page.id" :class="`w-full rounded px-3 py-2 flex items-center justify-start text-xs font-semibold ${setid===page.id?'bg-light text-white':'text-lightest'}`">
-            <i class="material-icons mr-">
+          <button v-for="page in pages" @click="setid=page.id" :class="`text-white w-full rounded px-3 py-2 flex items-center justify-start text-xs font-semibold ${setid===page.id?'bg-gray-600 text-white':'text-lightest'}`">
+            <i class="material-symbols-outlined mr-3">
               {{ page.icon }}
             </i>
             <p class="hover:opacity-100 text-sm text-white font-semibold">{{ page.name }}</p>
@@ -51,24 +53,24 @@
         <div class="mx-5">
           <h1 class="mb-3 text-xs text-lightest tracking-widest uppercase">playlist</h1>
           <button class="flex items-center justify-start opacity-75 hover:opacity-100">
-            <img src="" alt="" class="h-8 w-8 mr-3">
+            <img src="./assets/image/create.png" alt="" class="h-8 w-8 mr-3">
             <p class="text-sm text-white font-semibold">Create Playlist</p>
           </button>
           <button class="flex items-center justify-start opacity-75 hover:opacity-100">
-            <img src="" alt="" class="h-8 w-8 mr-3">
+            <img src="./assets/image/liked.jpg" alt="" class="h-8 w-8 mr-3">
             <p class="text-sm text-white font-semibold">Liked Song</p>
           </button>
+          <div class="h-px 2-full bg-white my-3"></div>
           <button class="flex items-center justify-start opacity-75 hover:opacity-100">
-            <img src="" alt="" class="h-8 w-8 mr-3">
-            <p class="text-sm text-white font-semibold">Create Playlist</p>
+            <p class="text-sm text-white font-semibold">Night Music</p>
           </button>
           <div class="h-px w-full bg-light my-3"></div>
         </div>
         <div class="mx-5">
-          <div v-for="album in albums" class="w-full h-24 overflow-y-scroll"><p class="hover:text-white text-sm text-white font-semibold py-1">{{ album.name }}</p></div>
+          <div v-for="album in albums" class="w-full h-20 overflow-y-scroll"><p class="hover:text-white text-sm text-white font-semibold py-1">{{ album.name }}</p></div>
         </div>
-        <button class="flex items-center justify-start text-lightest hover:text-white">
-          <i class="material-icons mr-3 rounded-full border border-lightest">arrow_downward</i>
+        <button class="flex items-center justify-start text-gray-300 hover:text-white">
+          <i class="material-symbols-outlined mr-3 rounded-full border border-lightest">arrow_downward</i>
           <p class="text-sm font-semibold">Install App</p>
         </button>
       </div>
@@ -76,7 +78,7 @@
         <img src="" alt="">
         <div class="w-full h-full flex justify-end items-start bg-yellow-500 absolute">
           <div class="bg-black rounded-full h-8 w-8">
-            <i class="material-icons text-white">keyboard_arrow_down</i>
+            <i class="material-symbols-outlined text-white">keyboard_arrow_down</i>
           </div>
         </div>
       </div>
@@ -85,17 +87,17 @@
         <div class="w-full sticky top-0 p-2 flex items-center justify-between">
           <div class="flex items-center">
             <button class="rounded-full bg-black w-8 h-8 text-white">
-              <i class="material-icons text-xl mr-3">keyboard_arrow_left</i>
+              <i class="material-symbols-outlined text-xl mr-3">keyboard_arrow_left</i>
             </button>
             <button class="rounded-full bg-black w-8 h-8 text-white">
-              <i class="material-icons text-xl">keyboard_arrow_right</i>
+              <i class="material-symbols-outlined text-xl">keyboard_arrow_right</i>
             </button>
           </div>
           <div class="relative">
               <button class="bg-light rounded-full py-1 px-2 flex items-center" @click="showdropdown = true">
                 <img class="rounded-full h-6 w-6" src="" alt="">
                 <p class="text-white font-semibold text-xs">fiq</p>
-                <i class="material-icons text-white">arrow_drop_down</i>
+                <i class="material-symbols-outlined text-white">arrow_drop_down</i>
               </button>
               <div class="absolute bg-light w-full rounded mt" v-if="showdropdown === true">
                 <button class="w-full text-sm py-2 text-lightest hover text-white border-b border-light" @click="showdropdown = false">Account</button>
@@ -107,11 +109,11 @@
           <h1 class="pl-2 text-2xl font-semibold text-white tracking-wider hover:underline">Recently Played</h1>
           <h2 class="pr-8 pt-4 text-xs text-lightest uppercase tracking-wider hover:underline">See All</h2>
           <div class="w-full flex-wrap">
-            <div class="p-2 w-48">
+            <div v-for="recent in recents" class="p-2 w-48">
               <div class="bg-light w-full h-auto p-5">
-                <img src="" alt="" class="h-auto w-full shadow mb-2">
-                <h1 class="text-sm font-semibold text-white tracking-wide"></h1>
-                <h2 class="text-xs text-lightest tracking-wide"></h2>
+                <img :src="`${recent.src}`" alt="" class="h-auto w-full shadow mb-2">
+              <h1 class="text-sm font-semibold text-white tracking-wide">{{ recent.title }}</h1>
+                <h2 class="text-xs text-lightest tracking-wide">{{ recent.artist }}</h2>
               </div>
             </div>
           </div>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import '@material/web/button/filled-button.js';
-import '@material/web/button/outlined-button.js';
-import '@material/web/checkbox/checkbox.js';
 
 createApp(App).mount('#app')
