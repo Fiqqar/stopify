86bb017a127c3314956ebafe8f4e8afcf195b9fc
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-09-15 11:29:00 +0700

    15-9-24 (fix styling for the main content and navigation)
    done
    -adding shadow to recently played
    -fixing navigation bar
    -change background color to zinc

--- src/App.vue
+++ src/App.vue
@@ -37,12 +37,12 @@
   <div class="bg-black" style="height:100vh;">
     <div class="flex" style="height:88vh;">
       <!-- nav -->
-      <div class="w-56 bg-black h-full">
+      <div class="w-72 bg-black h-full">
         <div class="p-6">
           <img src="./assets/logoname.png" alt="" class="h-20">
         </div>
         <div class="mx-2">
-          <button v-for="page in pages" @click="setid=page.id" :class="`text-white w-full rounded px-3 py-2 flex items-center justify-start text-xs font-semibold ${setid===page.id?'bg-gray-600 text-white':'text-lightest'}`">
+          <button v-for="page in pages" @click="setid=page.id" :class="`ml-2 text-white rounded px-3 py-2 w-56 flex items-center justify-start text-xs font-semibold ${setid===page.id?'bg-gray-600 text-white':'text-lightest'}`">
             <i class="material-symbols-outlined mr-3">
               {{ page.icon }}
             </i>
@@ -50,18 +50,18 @@
           </button>
         </div>
 
-        <div class="mx-5">
+        <div class="mx-2">
           <h1 class="mb-3 text-xs text-lightest tracking-widest uppercase">playlist</h1>
-          <button class="flex items-center justify-start opacity-75 hover:opacity-100">
+          <button class="flex items-center justify-start opacity-75 hover:opacity-100 ml-2 w-56 mb-2">
             <img src="./assets/image/create.png" alt="" class="h-8 w-8 mr-3">
             <p class="text-sm text-white font-semibold">Create Playlist</p>
           </button>
-          <button class="flex items-center justify-start opacity-75 hover:opacity-100">
+          <button class="flex items-center justify-start opacity-75 hover:opacity-100 ml-2 w-56">
             <img src="./assets/image/liked.jpg" alt="" class="h-8 w-8 mr-3">
             <p class="text-sm text-white font-semibold">Liked Song</p>
           </button>
           <div class="h-px 2-full bg-white my-3"></div>
-          <button class="flex items-center justify-start opacity-75 hover:opacity-100">
+          <button class="flex items-center justify-start opacity-75 hover:opacity-100 ml-2 w-56">
             <p class="text-sm text-white font-semibold">Night Music</p>
           </button>
           <div class="h-px w-full bg-light my-3"></div>
@@ -83,19 +83,19 @@
         </div>
       </div>
       <!-- main content -->
-       <div class="w-full h-full relative bg-blue-400">
+       <div class="w-full h-full relative bg-zinc-900">
         <div class="w-full sticky top-0 p-2 flex items-center justify-between">
           <div class="flex items-center">
-            <button class="rounded-full bg-black w-8 h-8 text-white">
-              <i class="material-symbols-outlined text-xl mr-3">keyboard_arrow_left</i>
+            <button class="rounded-full bg-black w-16 h-11 mr-1 text-white">
+              <i class="material-symbols-outlined">keyboard_arrow_left</i>
             </button>
-            <button class="rounded-full bg-black w-8 h-8 text-white">
-              <i class="material-symbols-outlined text-xl">keyboard_arrow_right</i>
+            <button class="rounded-full bg-black w-16 h-11 text-white">
+              <i class="material-symbols-outlined">keyboard_arrow_right</i>
             </button>
           </div>
           <div class="relative">
-              <button class="bg-light rounded-full py-1 px-2 flex items-center" @click="showdropdown = true">
-                <img class="rounded-full h-6 w-6" src="" alt="">
+              <button class="bg-light rounded-full py-1 px-2 flex items-center w-24" @click="showdropdown = true">
+                <img class="rounded-full h-6 w-6 mr-4" src="/src/assets/image/profile.png" alt="">
                 <p class="text-white font-semibold text-xs">fiq</p>
                 <i class="material-symbols-outlined text-white">arrow_drop_down</i>
               </button>
@@ -105,15 +105,17 @@
               </div>
             </div>
         </div>
-        <div class="px-6 py-3 flex items-center container justify-between">
-          <h1 class="pl-2 text-2xl font-semibold text-white tracking-wider hover:underline">Recently Played</h1>
-          <h2 class="pr-8 pt-4 text-xs text-lightest uppercase tracking-wider hover:underline">See All</h2>
-          <div class="w-full flex-wrap">
-            <div v-for="recent in recents" class="p-2 w-48">
-              <div class="bg-light w-full h-auto p-5">
+        <div>
+          <div class="px-6 py-3 flex items-center justify-between">
+            <h1 class="pl-2 text-2xl font-semibold text-white tracking-wider hover:underline">Recently Played</h1>
+            <h2 class="pr-8 pt-4 text-xs text-white uppercase mb-3 tracking-wider hover:underline">See All</h2>
+          </div>
+          <div class="w-full flex flex-wrap">
+            <div v-for="recent in recents" class="p-2 w-48 ml-4">
+              <div class="bg-zinc-800 hover:bg-zinc-700 w-50 h-60 p-5 rounded-lg hover:shadow-md hover:shadow-slate-700">
                 <img :src="`${recent.src}`" alt="" class="h-auto w-full shadow mb-2">
               <h1 class="text-sm font-semibold text-white tracking-wide">{{ recent.title }}</h1>
-                <h2 class="text-xs text-lightest tracking-wide">{{ recent.artist }}</h2>
+                <h2 class="text-xs text-slate-400 tracking-wide">{{ recent.artist }}</h2>
               </div>
             </div>
           </div>
