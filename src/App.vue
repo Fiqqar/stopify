e9f349e4f02ddfa3ef2065bc367a9c9672ef64a7
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-09-20 20:33:12 +0700

    20-9-24 (adding some new section and star editing playbar))
    done

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
     name: "App",
     data : function () {
       return {
+        showdropdown: false,
+        setid: "home",
+        username : "fiq",
         pages: [
           {id:"home", name:"Home", icon:"home"},
           {id:"search", name:"Search", icon:"search"},
           {id:"library", name:"Library", icon:"bar_chart"},
         ],
-        setid: "home",
         albums: [
           {name: "Nonfiksi"},
           {name: "Lagipula Hidup Akan Berakhir"},
@@ -17,7 +19,6 @@
           {name: "Timeline"},
           {name: "Going Going Gone"},
         ],
-        showdropdown: false,
         recents: [
           {src: "/src/assets/image/lampu_kuning.jpeg", title: "Lampu Kuning", artist: "Juicy Luicy"},
           {src: "/src/assets/image/berdansalah.jpeg", title: "Berdansalah, Karir Ini Tak Ada Artinya", artist: "Hindia"},
@@ -25,6 +26,14 @@
           {src: "/src/assets/image/me_myself.jpeg", title: "Me Myself and Dollar Hell", artist: "Mild High Club"},
           {src: "/src/assets/image/no_suprises.jpg", title: "No Suprises", artist: "Radiohead"},
           {src: "/src/assets/image/untitled.jpeg", title: "Untitled", artist: "Rex Orange Country"}
+        ],
+        foryous: [
+          {src: "src/assets/image/nonfiksi.jpeg", title: "Nonfiksi"},
+          {src: "src/assets/image/sentimental.jpeg", title: "Sentimental"},
+          {src: "src/assets/image/duotones.jpeg", title: "Duotones"},
+          {src: "src/assets/image/themoment.jpeg", title: "The Moment"},
+          {src: "src/assets/image/insyaallah.jpeg", title: "Insya Allah"},
+          {src: "src/assets/image/sentimentalsideA.jpeg", title: "Sentimental: Side A"}
         ]
       }
     }
@@ -96,7 +105,7 @@
           <div class="relative">
               <button class="bg-light rounded-full py-1 px-2 flex items-center w-24" @click="showdropdown = true">
                 <img class="rounded-full h-6 w-6 mr-4" src="/src/assets/image/profile.png" alt="">
-                <p class="text-white font-semibold text-xs">fiq</p>
+                <p class="text-white font-semibold text-xs">{{ username }}</p>
                 <i class="material-symbols-outlined text-white">arrow_drop_down</i>
               </button>
               <div class="absolute bg-light w-full rounded mt" v-if="showdropdown === true">
@@ -111,7 +120,7 @@
             <h2 class="pr-8 pt-4 text-xs text-white uppercase mb-3 tracking-wider hover:underline">See All</h2>
           </div>
           <div class="w-full flex flex-wrap">
-            <div v-for="recent in recents" class="p-2 w-48 ml-4">
+            <div v-for="recent in recents" class="p-2 w-48 ml-6">
               <div class="bg-zinc-800 hover:bg-zinc-700 w-50 h-60 p-5 rounded-lg hover:shadow-md hover:shadow-slate-700">
                 <img :src="`${recent.src}`" alt="" class="h-auto w-full shadow mb-2">
               <h1 class="text-sm font-semibold text-white tracking-wide">{{ recent.title }}</h1>
@@ -120,11 +129,29 @@
             </div>
           </div>
         </div>
+        <div class="px-6 py-3">
+          <div class="pl-2 ">
+            <h1 class="text-2xl font-semibold text-white tracking-wider hover:underline">Made for {{ username }}</h1>
+            <h2 class="pr-8 pt-4 text-xs text-white uppercase mb-3 tracking-wider hover:underline">Get Better Recommendations the more you listen</h2>
+          </div>
+          <div class="w-full flex flex-wrap">
+            <div v-for="foryou in foryous" class="p-2 w-48 mr-6">
+              <div class="bg-zinc-800 hover:bg-zinc-700 w-50 h-50 p-5 rounded-lg hover:shadow-md hover:shadow-slate-700">
+                <img :src="`${foryou.src}`" alt="" class="h-auto w-full shadow mb-2">
+              <h1 class="text-sm font-semibold text-white tracking-wide">{{ foryou.title }}</h1>
+              </div>
+            </div>
+          </div>
+        </div>
        </div>
     </div>
     <!-- play bar -->
-     <div class="w-full bg-light">
-      
+     <div class="w-full bg-zinc-900 flex items-center justify-between px-3 h-64">
+      <div class="flex items-center">
+        <h1 class="text-sm text-white tracking-wide">Terlalu Tinggi</h1>
+        <h2 class="text-xs text-white tracking-wide">Juicy Luicy</h2>
+      </div>
+      <i class="material-symbols-outlined text-blue-400 mx-4">favorite</i>
      </div>
   </div>
 </template>
